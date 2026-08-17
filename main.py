8c260af85ec7cfc544704689664da90e52a7c55d
Felipe Arias <felipemaximoarias@gmail.com>
2026-08-17 00:25:36 -0300

    Atributos de las clases Persona, HistorialResponsable y TipoObservacion

--- main.py
+++ main.py
@@ -17,3 +17,21 @@ class Raza(BaseModel):
     paisOrigen: str = Field(..., description="Pais de origen de la raza")
 
 
+class Persona(BaseModel):
+    nombre: str = Field(..., description="Nombre de la persona")
+    apellido: str = Field(..., description="Apellido de la persona")
+    dni: str = Field(..., description="DNI de la persona")
+    telefono: str = Field(..., description="Telefono de contacto")
+    email: str = Field(..., description="Email de contacto")
+
+
+class HistorialResponsable(BaseModel):
+    fechaInicio: date = Field(..., description="Fecha de inicio del vinculo")
+    fechaFin: Optional[date] = Field(default=None, description="Fecha de fin del vinculo, si ya finalizo")
+    persona: Persona = Field(..., description="Persona responsable en este periodo")
+
+
+class TipoObservacion(BaseModel):
+    nombre: str = Field(..., description="Nombre del tipo de observacion")
+    descripcion: str = Field(..., description="Descripcion del tipo de observacion")
+
